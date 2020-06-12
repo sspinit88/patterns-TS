315e43d80244f3f0e24ae01cc77a33ad5741e3b2
Sergey Pashnin <sspinit88@gmail.com>
2020-06-12 17:09:27 +0300

    Template method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+"use strict";
 /*
-* описывем интерфейсы
-* ингредиентов
+* Шаблонный Метиод определяет основные шаги алгоритма и позволяет субклассам предоставить реализацию
+* одного или нескольких шагов.
 * */
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
@@ -15,148 +16,52 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-/*
-* создаем классы ингредиентов,
-* которые будут использоваться
-* при приготовлении пиццы в NY
-* */
-var DoughNY = /** @class */ (function () {
-    function DoughNY() {
-        this.fakeField = 'Dough';
+var CaffeineBeverage = /** @class */ (function () {
+    function CaffeineBeverage() {
     }
-    return DoughNY;
+    CaffeineBeverage.prototype.prepareRecipe = function () {
+        this.boilWater();
+        this.brew();
+        this.pourInCup();
+        this.addCondiments();
+    };
+    CaffeineBeverage.prototype.boilWater = function () {
+        console.log('Вода нагрета.');
+    };
+    CaffeineBeverage.prototype.pourInCup = function () {
+        console.log('Налито в чашку');
+    };
+    return CaffeineBeverage;
 }());
-var SauceNY = /** @class */ (function () {
-    function SauceNY() {
-        this.fakeField = 'Sauce';
+var Tea = /** @class */ (function (_super) {
+    __extends(Tea, _super);
+    function Tea() {
+        return _super.call(this) || this;
     }
-    return SauceNY;
-}());
-var VeggiesNY = /** @class */ (function () {
-    function VeggiesNY() {
-        this.fakeField = 'Veggies';
+    Tea.prototype.boilWater = function () {
+        console.log('Вода нагрета до 50 градусов.');
+    };
+    Tea.prototype.brew = function () {
+        console.log('Чай заваривается.');
+    };
+    Tea.prototype.addCondiments = function () {
+        console.log('В чай добавлен лимон');
+    };
+    return Tea;
+}(CaffeineBeverage));
+var Coffee = /** @class */ (function (_super) {
+    __extends(Coffee, _super);
+    function Coffee() {
+        return _super.call(this) || this;
     }
-    return VeggiesNY;
-}());
-var CheeseNY = /** @class */ (function () {
-    function CheeseNY() {
-        this.fakeField = 'Cheese';
-    }
-    return CheeseNY;
-}());
-var PepperoniNY = /** @class */ (function () {
-    function PepperoniNY() {
-        this.fakeField = 'Pepperoni';
-    }
-    return PepperoniNY;
-}());
-var ClamNY = /** @class */ (function () {
-    function ClamNY() {
-        this.fakeField = 'Clam';
-    }
-    return ClamNY;
-}());
-/*
-* абстрактный класс пиццы
-* */
-var Pizza = /** @class */ (function () {
-    function Pizza() {
-        /*
-        * каждый объект пиццы содержит набор
-        * ингредиентов, используемых при ее
-        * приготовлении
-        * */
-        this.name = 'no data';
-        this.dough = { fakeField: 'no data' };
-        this.sauce = { fakeField: 'no data' };
-        this.veggies = [];
-        this.cheese = { fakeField: 'no data' };
-        this.pepperoni = { fakeField: 'no data' };
-        this.clam = { fakeField: 'no data' };
-    }
-    /*
-    * остальные методы остаются неизменными
-    * */
-    Pizza.prototype.bake = function () {
-        console.log('Bake for 25 min at 350');
+    Coffee.prototype.brew = function () {
+        console.log('Кофе заваривается.');
     };
-    Pizza.prototype.cut = function () {
-        console.log('Cutting the pizza into diagonal slices');
+    Coffee.prototype.addCondiments = function () {
+        console.log('Добавлено молоко.');
+        console.log('Добавлен сахар.');
     };
-    Pizza.prototype.box = function () {
-        console.log('Place pizza in official PizzaStore box');
-    };
-    Pizza.prototype.setName = function (n) {
-        this.name = n;
-    };
-    Pizza.prototype.getName = function () {
-        console.log("Pizza's name: " + this.name);
-        return this.name;
-    };
-    return Pizza;
-}());
-/*
-* NYPizzaIngredientFactory реализует
-* общий интерфейс PizzaIngredientFactory
-* */
-var NYPizzaIngredientFactory = /** @class */ (function () {
-    function NYPizzaIngredientFactory() {
-    }
-    /*
-    * для каждого ингредиента
-    * в семействе создается
-    * его версия для NY, для
-    * других городов будет свой
-    * набор ингредиентов
-    * */
-    NYPizzaIngredientFactory.prototype.createCheese = function () {
-        return new CheeseNY();
-    };
-    NYPizzaIngredientFactory.prototype.createClam = function () {
-        return new ClamNY();
-    };
-    NYPizzaIngredientFactory.prototype.createDough = function () {
-        return new DoughNY();
-    };
-    NYPizzaIngredientFactory.prototype.createPepperoni = function () {
-        return new PepperoniNY();
-    };
-    NYPizzaIngredientFactory.prototype.createSauce = function () {
-        return new SauceNY();
-    };
-    NYPizzaIngredientFactory.prototype.createVeggies = function () {
-        return [{ fakeField: 'v1' }, { fakeField: 'v2' }];
-    };
-    return NYPizzaIngredientFactory;
-}());
-/*
-*
-* */
-var CheesePizza = /** @class */ (function (_super) {
-    __extends(CheesePizza, _super);
-    function CheesePizza(ingredientFactory) {
-        var _this = _super.call(this) || this;
-        _this.ingredientFactory = ingredientFactory;
-        return _this;
-    }
-    /*
-    * prepare() - готовит пиццу с сыром. Когда ему требуется
-    * очередной ингредиент, он запрашивает его у фабрики
-    * */
-    CheesePizza.prototype.prepare = function () {
-        this.dough = this.ingredientFactory.createDough();
-        this.sauce = this.ingredientFactory.createSauce();
-        this.cheese = this.ingredientFactory.createCheese();
-    };
-    return CheesePizza;
-}(Pizza));
-/*
-* пример:
-* */
-var NYCheesePizza = new CheesePizza(new NYPizzaIngredientFactory());
-NYCheesePizza.prepare();
-NYCheesePizza.bake();
-NYCheesePizza.cut();
-NYCheesePizza.box();
-NYCheesePizza.setName('NYCheesePizza');
-NYCheesePizza.getName();
+    return Coffee;
+}(CaffeineBeverage));
+new Tea().prepareRecipe();
+new Coffee().prepareRecipe();
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,72 @@
-class MyClass {
+/*
+* Шаблонный Метиод определяет основные шаги алгоритма и позволяет субклассам предоставить реализацию
+* одного или нескольких шагов.
+* */
 
-  private static instance: any = undefined;
+abstract class CaffeineBeverage {
+
+  protected constructor() {
+  }
+
+  prepareRecipe(): void {
+    this.boilWater();
+    this.brew();
+    this.pourInCup();
+    this.addCondiments();
+  }
+
+  protected boilWater(): void {
+    console.log('Вода нагрета.');
+  }
+
+  protected pourInCup(): void {
+    console.log('Налито в чашку');
+  }
+
+  abstract brew(): void ;
+
+  abstract addCondiments(): void;
+
+}
+
+class Tea extends CaffeineBeverage {
 
   constructor() {
+    super();
   }
 
-  static getInstance(dataForInstance: any): any {
-    if (this.instance == null) {
-      this.instance = dataForInstance;
-    }
-
-    return this.instance;
+  // метод можно переопределить
+  boilWater(): void {
+    console.log('Вода нагрета до 50 градусов.');
   }
 
+  brew(): void {
+    console.log('Чай заваривается.')
+  }
 
-  // другие методы...
+  addCondiments(): void {
+    console.log('В чай добавлен лимон');
+  }
+
 }
+
+class Coffee extends CaffeineBeverage {
+
+  constructor() {
+    super();
+  }
+
+  brew(): void {
+    console.log('Кофе заваривается.');
+  }
+
+  addCondiments(): void {
+    console.log('Добавлено молоко.');
+    console.log('Добавлен сахар.');
+  }
+
+}
+
+
+new Tea().prepareRecipe();
+new Coffee().prepareRecipe();
